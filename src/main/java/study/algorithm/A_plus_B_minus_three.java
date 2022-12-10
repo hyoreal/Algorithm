84ca348f2f433a09750011eb5702d963cb15147f
hyojin <hyoreal51@gmail.com>
2022-12-10 22:47:44 +0900

    loop - A+B-3

---
+++ src/main/java/study/algorithm/A_plus_B_minus_three.java
@@ -0,0 +1,35 @@
+package study.algorithm;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/**
+ * 두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
+ *
+ * 첫째 줄에 테스트 케이스의 개수 T가 주어진다.
+ *
+ * 각 테스트 케이스는 한 줄로 이루어져 있으며, 각 줄에 A와 B가 주어진다. (0 < A, B < 10)
+ */
+
+public class A_plus_B_minus_three {
+    public static void main(String[] args) throws IOException {
+
+        // TODO: 테스트케이스 개수 T, 각 줄마다 A와 B
+        // scanner 대신 bufferReader. 문자열을 한 줄로 읽음. 문자열 분리 위한 stringTokenizer 사용
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int T = Integer.parseInt(br.readLine()); // 테스트 케이스 개수
+
+        StringBuilder sb = new StringBuilder(); // 출력 시 모든 답을 함께 출력하기 위한 StringBuilder
+
+        for (int i = 0; i < T; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+            int A = Integer.parseInt(st.nextToken());
+            int B = Integer.parseInt(st.nextToken());
+            sb.append(A+B).append("\n");
+        }
+        System.out.println(sb);
+    }
+}
