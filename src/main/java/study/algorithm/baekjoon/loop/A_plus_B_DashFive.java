ae35d67e3acf5f93400c5d6f71b2e5e593cf7e25
hyojin <hyoreal51@gmail.com>
2022-12-11 01:58:32 +0900

    loop - A+B-5

---
+++ src/main/java/study/algorithm/baekjoon/loop/A_plus_B_DashFive.java
@@ -0,0 +1,36 @@
+package study.algorithm.baekjoon.loop;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+/**
+ * 두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
+ *
+ * 입력은 여러 개의 테스트 케이스로 이루어져 있다.
+ *
+ * 각 테스트 케이스는 한 줄로 이루어져 있으며, 각 줄에 A와 B가 주어진다. (0 < A, B < 10)
+ *
+ * 입력의 마지막에는 0 두 개가 들어온다.
+ */
+
+public class A_plus_B_DashFive {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        while (true) {
+            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+            int A = Integer.parseInt(st.nextToken());
+            int B = Integer.parseInt(st.nextToken());
+
+            if (A == 0 && B == 0) {
+                break;
+            }
+
+            bw.write(A + B + "\n");
+        }
+        bw.flush();
+        bw.close();
+    }
+}
